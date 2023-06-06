a49ed6ed7d16e952423ad6ceaa4742944d369fa8
Yunis Isnaini <yunisyou30@gmail.com>
2023-06-06 16:04:38 +0700

    Form+Forget+Reset+Menambahkan Route App

---
+++ src/components/Form/FormForget.jsx
@@ -0,0 +1,54 @@
+import "./FormLogin.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+function FormForget() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const passwordInputType = passwordVisible ? "text" : "password";
+
+  return (
+    <>
+      <h1 className="fw-bold mb-4">Forget Password</h1>
+
+      <form onSubmit={(e) => e.preventDefault}>
+        <div>
+          <p className="mb-1">Email/No telepon</p>
+        </div>
+        <div className="input-group mb-3">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Contoh: johndoe@gmail.com"
+            aria-label="Email"
+            // value={email}
+            onChange={(e) => e.preventDefault}
+            required
+            style={{ fontFamily: "Poppins" }}
+          />
+        </div>
+
+        {/* {error && <p className="error-message">{error}</p>} */}
+        <div className="d-grid gap-2 mt-4">
+          <button className="btn btn-primary lg sign-up" type="submit">
+            Reset Password
+          </button>
+        </div>
+      </form>
+      <p className="mt-5 mb-1 text-center">
+        Belum punya akun?{"  "}
+        <Link to="/register" className="fw-bold register">
+          Daftar di sini
+        </Link>
+      </p>
+    </>
+  );
+}
+
+export default FormForget;
---
+++ src/pages/Forget.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import loginLogo from "/Group 84.svg";
+import FormForget from "../components/Form/FormForget";
+
+function Register() {
+  const styles = `
+  .container-fluid {
+    padding-left:0px;
+  }
+  `;
+  return (
+    <>
+      <style>{styles}</style>
+      <div className="container-fluid d-flex flex-column justify-content-end">
+        <div className="row align-items-center">
+          <div className="col-md-6">
+            <img
+              src={loginLogo}
+              alt="background"
+              style={{
+                width: "100%",
+                maxHeight: "100vh", // Atur tinggi maksimal gambar agar sesuai dengan tinggi layar
+                objectFit: "cover",
+              }}
+            />
+          </div>
+
+          <div className="col-md-5 ms-5 me-xxl-5 ps-xxl-4">
+            <FormForget />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Register;

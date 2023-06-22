568feb4a18d26af1ab16980378f9751072171db8
Danarzlf <Danarzlf10@example.com>
2023-06-23 03:04:35 +0700

    feat/navbar-login

--- src/pages/Flight.jsx
+++ src/pages/Flight.jsx
@@ -1,4 +1,5 @@
 import "./Flight.css";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import SelectDay from "../components/Filter/SelectDay";
 import Filter from "../components/Filter/Filter";
@@ -7,17 +8,27 @@ import NavbarHomepage from "../components/NavbarHomepage";
 import Notfound from "../components/Notfound";
 import Result from "../components/Result";
 import SoldoutComponent from "../components/Soldout";
+import NavbarUser from "../components/NavbarUser";
 import MyModal from "../components/Beranda/MyModal";
 
 function Flight() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // State untuk menyimpan status login pengguna
+
+  useEffect(() => {
+    // Cek apakah pengguna sudah login atau memiliki token di lokal
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true); // Jika ada token, pengguna dianggap sudah login
+    }
+  }, []);
   return (
     <>
-      <NavbarHomepage />
+      {isLoggedIn ? <NavbarUser /> : <NavbarHomepage />}
       <div className="container-fluid" id="select-flight">
         <h4 className="title">Pilih Penerbangan</h4>
         <SelectDay />
         <div className="d-flex justify-content-end">
-          <MyModal/>
+          <MyModal />
         </div>
         <div className="row filter-loading mt-4">
           <div className="col-3 filter-l">

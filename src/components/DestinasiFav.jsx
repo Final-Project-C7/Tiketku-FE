47f7c29b46d1968a89cb0735d6f9a8ffd0c6773c
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-06-28 03:54:54 +0700

    update responsive Payment

--- src/components/DestinasiFav.jsx
+++ src/components/DestinasiFav.jsx
@@ -5,25 +5,17 @@ import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./DestinasiFav.css";
 
-const DestinasiFav = ({ destinations, onFilter }) => {
-  const [searchQuery, setSearchQuery] = useState('');
+const DestinasiFav = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
 
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
   };
-
-  const handleApplyFilter = () => {
-    const filteredDestinations = destinations.filter((destination) =>
-      destination.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    onFilter(filteredDestinations);
-  };
-
   return (
     <div className="destinasi-fav position-relative d-flex flex-column mx-auto">
-      <h5 className="fw-bold">Destinasi Favorit</h5>
-      <div className="d-flex mb-5">
-        <Button className="destinasi-fav__category-2 d-flex align-items-center justify-content-center text-dark text-decoration-none mt-2 p-1">
+      <h5 className="fw-bold pt-0">Destinasi Favorit</h5>
+      <div className="destinasi-fav__cat d-flex mb-5">
+        {/* <Button className="destinasi-fav__category-2 d-flex align-items-center justify-content-center text-dark text-decoration-none mt-2 p-1">
           <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
           <div 
           onClick={handleApplyFilter}
@@ -89,96 +81,120 @@ const DestinasiFav = ({ destinations, onFilter }) => {
           placeholder="Semua"
           >
           Afrika</div>
-        </Button>
+        </Button> */}
 
-        {/* <Link to="/" className="destinasi-fav__category-1 d-flex align-items-center justify-content-center text-white text-decoration-none mt-2 p-1">
+        <Link
+          to="/"
+          className={`${selectedCategory === "" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-sm-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          onClick={() => handleCategoryClick("")}
+        >
           <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
           Semua
         </Link>
 
-        <Link to="/" className="destinasi-fav__category-2 d-flex align-items-center justify-content-center text-decoration-none ms-3 mt-2 p-1">
+        <Link
+          to="?cat=Asia"
+          className={`${selectedCategory === "Asia" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-sm-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          onClick={() => handleCategoryClick("Asia")}
+        >
           <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
           Asia
         </Link>
-        <Link to="/" className="destinasi-fav__category-2 d-flex align-items-center justify-content-center text-decoration-none ms-3 mt-2 p-1">
+
+        <Link
+          to="?cat=Amerika"
+          className={`${selectedCategory === "Amerika" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-sm-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          onClick={() => handleCategoryClick("Amerika")}
+        >
           <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
           Amerika
         </Link>
-        <Link to="/" className="destinasi-fav__category-2 d-flex align-items-center justify-content-center text-decoration-none ms-3 mt-2 p-1">
+        <Link
+          to="?cat=Australia"
+          className={`${selectedCategory === "Australia" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-sm-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          onClick={() => handleCategoryClick("Australia")}
+        >
           <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
           Australia
         </Link>
-        <Link to="/" className="destinasi-fav__category-2 d-flex align-items-center justify-content-center text-decoration-none ms-3 mt-2 p-1">
+        <Link
+          to="?cat=Eropa"
+          className={`${selectedCategory === "Eropa" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-sm-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          onClick={() => handleCategoryClick("Eropa")}
+        >
           <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
           Eropa
         </Link>
-        <Link to="/" className="destinasi-fav__category-2 d-flex align-items-center justify-content-center text-decoration-none ms-3 mt-2 p-1">
+        <Link
+          to="?cat=Afrika"
+          className={`${selectedCategory === "Afrika" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-sm-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          onClick={() => handleCategoryClick("Afrika")}
+        >
           <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
           Afrika
-        </Link> */}
-
+        </Link>
       </div>
-      
+
       <Container>
         <Row className="mb-5">
           <Col className="col col-3 d-flex">
-          <Card className="shadow" style={{ width: "95%" }}>
-            <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
-            <Card.Body className="pt-2">
-              <Card.Text className="destinasi-card__text-1 fw-bold mb-1">Jakarta &rarr; Bangkok</Card.Text>
-              <Card.Text className="destinasi-card__text-2 fw-bold mb-1">AirAsia</Card.Text>
-              <Card.Text className="destinasi-card__text-2 mb-1 text-black">20 - 30 Maret 2023</Card.Text>
-              <Card.Text className="destinasi-card__text-1 mb-1 text-black">
-                Mulai dari <span className="text-danger fw-bold">IDR 950.000</span>
-              </Card.Text>
-            </Card.Body>
-          </Card>
+            <Card className="shadow" style={{ width: "95%" }}>
+              <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
+              <Card.Body className="pt-2">
+                <Card.Text className="destinasi-card__text-1 fw-bold mb-1">Jakarta &rarr; Bangkok</Card.Text>
+                <Card.Text className="destinasi-card__text-2 fw-bold mb-1">AirAsia</Card.Text>
+                <Card.Text className="destinasi-card__text-2 mb-1 text-black">20 - 30 Maret 2023</Card.Text>
+                <Card.Text className="destinasi-card__text-1 mb-1 text-black">
+                  Mulai dari <span className="text-danger fw-bold">IDR 950.000</span>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
 
           <Col className="col col-3 d-flex">
-          <Card className="shadow" style={{ width: "95%", heigh: "70%" }}>
-            <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
-            <Card.Body className="pt-2">
-              <Card.Text className="destinasi-card__text-1 fw-bold mb-1">Jakarta &rarr; Bangkok</Card.Text>
-              <Card.Text className="destinasi-card__text-2 fw-bold mb-1">AirAsia</Card.Text>
-              <Card.Text className="destinasi-card__text-2 mb-1 text-black">20 - 30 Maret 2023</Card.Text>
-              <Card.Text className="destinasi-card__text-1 mb-1 text-black">
-                Mulai dari <span className="text-danger fw-bold">IDR 950.000</span>
-              </Card.Text>
-            </Card.Body>
-          </Card>
+            <Card className="shadow" style={{ width: "95%", heigh: "70%" }}>
+              <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
+              <Card.Body className="pt-2">
+                <Card.Text className="destinasi-card__text-1 fw-bold mb-1">Jakarta &rarr; Bangkok</Card.Text>
+                <Card.Text className="destinasi-card__text-2 fw-bold mb-1">AirAsia</Card.Text>
+                <Card.Text className="destinasi-card__text-2 mb-1 text-black">20 - 30 Maret 2023</Card.Text>
+                <Card.Text className="destinasi-card__text-1 mb-1 text-black">
+                  Mulai dari <span className="text-danger fw-bold">IDR 950.000</span>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
 
           <Col className="col col-3 d-flex">
-          <Card className="shadow" style={{ width: "95%" }}>
-            <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
-            <Card.Body className="pt-2">
-              <Card.Text className="destinasi-card__text-1 fw-bold mb-1">Jakarta &rarr; Bangkok</Card.Text>
-              <Card.Text className="destinasi-card__text-2 fw-bold mb-1">AirAsia</Card.Text>
-              <Card.Text className="destinasi-card__text-2 mb-1 text-black">20 - 30 Maret 2023</Card.Text>
-              <Card.Text className="destinasi-card__text-1 mb-1 text-black">
-                Mulai dari <span className="text-danger fw-bold">IDR 950.000</span>
-              </Card.Text>
-            </Card.Body>
-          </Card>
+            <Card className="shadow" style={{ width: "95%" }}>
+              <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
+              <Card.Body className="pt-2">
+                <Card.Text className="destinasi-card__text-1 fw-bold mb-1">Jakarta &rarr; Bangkok</Card.Text>
+                <Card.Text className="destinasi-card__text-2 fw-bold mb-1">AirAsia</Card.Text>
+                <Card.Text className="destinasi-card__text-2 mb-1 text-black">20 - 30 Maret 2023</Card.Text>
+                <Card.Text className="destinasi-card__text-1 mb-1 text-black">
+                  Mulai dari <span className="text-danger fw-bold">IDR 950.000</span>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
 
           <Col className="col col-3 d-flex">
-          <Card className="shadow" style={{ width: "95%" }}>
-            <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
-            <Card.Body className="pt-2">
-              <Card.Text className="destinasi-card__text-1 fw-bold mb-1">Jakarta &rarr; Bangkok</Card.Text>
-              <Card.Text className="destinasi-card__text-2 fw-bold mb-1">AirAsia</Card.Text>
-              <Card.Text className="destinasi-card__text-2 mb-1 text-black">20 - 30 Maret 2023</Card.Text>
-              <Card.Text className="destinasi-card__text-1 mb-1 text-black">
-                Mulai dari <span className="text-danger fw-bold">IDR 950.000</span>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          </Col>  
+            <Card className="shadow" style={{ width: "95%" }}>
+              <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
+              <Card.Body className="pt-2">
+                <Card.Text className="destinasi-card__text-1 fw-bold mb-1">Jakarta &rarr; Bangkok</Card.Text>
+                <Card.Text className="destinasi-card__text-2 fw-bold mb-1">AirAsia</Card.Text>
+                <Card.Text className="destinasi-card__text-2 mb-1 text-black">20 - 30 Maret 2023</Card.Text>
+                <Card.Text className="destinasi-card__text-1 mb-1 text-black">
+                  Mulai dari <span className="text-danger fw-bold">IDR 950.000</span>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
 
-      {/* <div className="destinasi-card flex-wrap mb-5">
+        {/* <div className="destinasi-card flex-wrap mb-5">
         <Card className="shadow" style={{ width: "18.47%" }}>
           <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
           <Card.Body className="pt-2">

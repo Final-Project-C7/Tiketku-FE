d3b777a726d04187668285396caa626a174a1a2e
adikrnwn171 <adikrnwn171@gmail.com>
2023-06-28 01:11:21 +0700

    merge integration flight

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/flight" element={<Flight />} />
           <Route path="/soldout" element={<SelectFlight />} />
-          <Route path="/result" element={<Result />} />
+          <Route path="/result/:depart/:arrive" element={<Result />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/otp" element={<OTP />} />

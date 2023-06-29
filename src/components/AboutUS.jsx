d47e53460f1ddf3f6b288f861095b1de8d52e007
rizagithub <rizatul.20004@mhs.unesa.ac.id>
2023-06-29 16:37:52 +0700

    about us

--- src/components/AboutUS.jsx
+++ src/components/AboutUS.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 import NavbarHomepage from "../components/NavbarHomepage";
 import NavbarUser from "../components/NavbarUser";
+
+import "./AboutUs.css"
 
 const AboutUs = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -14,10 +16,57 @@ const AboutUs = () => {
     }
   }, []);
 
+  const style = `
+  .about-us{
+    padding-top:100px !important;
+    color:#4b1979 !important;
+    
+  }
+
+  .about-us__img{
+    width: 60%;
+
+    margin-right:20px;
+  }
+
+  .text__left{
+    padding-top:120px;
+  }
+
+  
+  `;
+
   return (
     <>
+      <style>{style}</style>
       {isLoggedIn ? <NavbarUser /> : <NavbarHomepage />}
-      <div className="about-us"></div>
+      <Container className="about">
+        <div className="about-us">
+          <h1 className="about-us__text text-center">T R A V E L E S I A</h1>
+          <h4 className="text-dark text-center">YOUR BEST TRAVELING PARTNER</h4>
+          <div className="row">
+            <div className="col-8">
+              <Image className="about-us__img mt-3" src="/pesawat2.jpg" alt="about us" />
+            </div>
+
+            <div className="text__left col-4 ms-0">  
+              {/* <h5>
+              Platform ini merupakan tempat beli dan booking tiket penerbangan secara online,<br/>
+              baik one way (sekali penerbangan) dan round trip (pulang pergi).<br/>
+              Platform ini membuka dan menyediakan berbagai jenis penerbangan domestik dan mancanegara dengan fitur dan harga terbaik di kelasnya.
+              </h5> */}
+
+            </div>
+
+          </div>
+          
+          
+          
+
+        </div>
+
+      </Container>
+      
     </>
   );
 };

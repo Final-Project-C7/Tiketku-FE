de4bdfa1f8a2318917a01f96adbf1f1ec7e7878d
Danarzlf <Danarzlf10@example.com>
2023-07-02 09:57:28 +0700

    feat/destination-filter

--- src/components/DestinasiFav.jsx
+++ src/components/DestinasiFav.jsx
@@ -7,6 +7,41 @@ import Moment from "moment";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./DestinasiFav.css";
 
+const dataWithContinent = [
+  { city: "Jakarta", continent: "Asia" },
+  { city: "Bali", continent: "Asia" },
+  { city: "Beijing", continent: "Asia" },
+  { city: "Florida", continent: "Amerika" },
+  { city: "Incheon", continent: "Asia" },
+  { city: "Sydney", continent: "Australia" },
+  { city: "Paris", continent: "Eropa" },
+  { city: "Surabaya", continent: "Asia" },
+  { city: "Incheon", continent: "Asia" },
+  { city: "Lombok", continent: "Asia" },
+  { city: "Tokyo", continent: "Asia" },
+  { city: "Singapura", continent: "Asia" },
+  { city: "Bangkok", continent: "Asia" },
+  { city: "New York", continent: "Amerika" },
+  { city: "Los Angeles", continent: "Amerika" },
+  { city: "Rio De Janeiro", continent: "Amerika" },
+  { city: "Miami", continent: "Amerika" },
+  { city: "Vancouver", continent: "Amerika" },
+  { city: "Sydney", continent: "Australia" },
+  { city: "Hobart", continent: "Australia" },
+  { city: "Cairns", continent: "Australia" },
+  { city: "Gold Coast", continent: "Australia" },
+  { city: "London", continent: "Eropa" },
+  { city: "Stockholm", continent: "Eropa" },
+  { city: "Moskow", continent: "Eropa" },
+  { city: "Amsterdam", continent: "Eropa" },
+  { city: "Berlin", continent: "Eropa" },
+  { city: "Roma", continent: "Eropa" },
+  { city: "Kairo", continent: "Afrika" },
+  { city: "Casablanca", continent: "Afrika" },
+  { city: "Johannesburg", continent: "Afrika" },
+  { city: "Lagos", continent: "Afrika" },
+];
+
 const DestinasiFav = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [data, setData] = useState("");
@@ -26,83 +61,153 @@ const DestinasiFav = () => {
       });
   }, []);
 
-  console.log(data?.data);
-
   return (
     <div className="destinasi-fav position-relative d-flex flex-column mx-auto">
       <h5 className="fw-bold pt-0">Destinasi Favorit</h5>
       <div className="destinasi-fav__cat d-flex mb-1">
         <Link
           to="/"
-          className={`${selectedCategory === "" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-md-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          className={`${
+            selectedCategory === ""
+              ? "destinasi-fav__category-1 px-3"
+              : "destinasi-fav__category-2 px-5 px-md-3"
+          } d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
           onClick={() => handleCategoryClick("")}
         >
-          <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
+          <Image
+            className="destinasi-fav__search-img me-2"
+            src="/search.svg"
+            alt="search"
+          />
           Semua
         </Link>
-
         <Link
-          to="?city=Asia"
-          className={`${selectedCategory === "Asia" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-md-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          to="?continent=Asia"
+          className={`${
+            selectedCategory === "Asia"
+              ? "destinasi-fav__category-1 px-3"
+              : "destinasi-fav__category-2 px-5 px-md-3"
+          } d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
           onClick={() => handleCategoryClick("Asia")}
         >
-          <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
+          <Image
+            className="destinasi-fav__search-img me-2"
+            src="/search.svg"
+            alt="search"
+          />
           Asia
         </Link>
-
         <Link
-          to="?city=Amerika"
-          className={`${selectedCategory === "Amerika" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-md-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          to="?continent=Amerika"
+          className={`${
+            selectedCategory === "Amerika"
+              ? "destinasi-fav__category-1 px-3"
+              : "destinasi-fav__category-2 px-5 px-md-3"
+          } d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
           onClick={() => handleCategoryClick("Amerika")}
         >
-          <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
+          <Image
+            className="destinasi-fav__search-img me-2"
+            src="/search.svg"
+            alt="search"
+          />
           Amerika
         </Link>
         <Link
-          to="?city=Australia"
-          className={`${selectedCategory === "Australia" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-md-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          to="?continent=Australia"
+          className={`${
+            selectedCategory === "Australia"
+              ? "destinasi-fav__category-1 px-3"
+              : "destinasi-fav__category-2 px-5 px-md-3"
+          } d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
           onClick={() => handleCategoryClick("Australia")}
         >
-          <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
+          <Image
+            className="destinasi-fav__search-img me-2"
+            src="/search.svg"
+            alt="search"
+          />
           Australia
         </Link>
         <Link
-          to="?city=Eropa"
-          className={`${selectedCategory === "Eropa" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-md-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          to="?continent=Eropa"
+          className={`${
+            selectedCategory === "Eropa"
+              ? "destinasi-fav__category-1 px-3"
+              : "destinasi-fav__category-2 px-5 px-md-3"
+          } d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
           onClick={() => handleCategoryClick("Eropa")}
         >
-          <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
+          <Image
+            className="destinasi-fav__search-img me-2"
+            src="/search.svg"
+            alt="search"
+          />
           Eropa
         </Link>
         <Link
-          to="?city=Afrika"
-          className={`${selectedCategory === "Afrika" ? "destinasi-fav__category-1 px-3" : "destinasi-fav__category-2 px-5 px-md-3"} d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
+          to="?continent=Afrika"
+          className={`${
+            selectedCategory === "Afrika"
+              ? "destinasi-fav__category-1 px-3"
+              : "destinasi-fav__category-2 px-5 px-md-3"
+          } d-flex align-items-center justify-content-center text-decoration-none mt-2 py-2 `}
           onClick={() => handleCategoryClick("Afrika")}
         >
-          <Image className="destinasi-fav__search-img me-2" src="/search.svg" alt="search" />
+          <Image
+            className="destinasi-fav__search-img me-2"
+            src="/search.svg"
+            alt="search"
+          />
           Afrika
         </Link>
       </div>
-
       <Container>
         <div className="destinasi-card d-flex justify-content-evenly flex-wrap mb-5 mx-auto">
-          {data?.data?.map((flight) => (
-            <Card className="destinasi-card__list shadow mt-3" key={flight.id}>
-              <Card.Img variant="top" className="p-2 pb-0" src="/Frame-152.svg" />
-              <Card.Body className="pt-2">
-                <Card.Text className="destinasi-card__text-1 fw-bold mb-1">
-                  {flight?.departureAirport?.city} &rarr; {flight?.arrivalAirport?.city}
-                </Card.Text>
-                <Card.Text className="destinasi-card__text-2 fw-bold mb-1">{flight.airline.airline_name}</Card.Text>
-                <Card.Text className="destinasi-card__text-2 mb-1 text-black">
-                  {Moment(flight.departure_time).format("DD")} - {Moment(flight.arrival_time).format("DD MMMM YYYY")}
-                </Card.Text>
-                <Card.Text className="destinasi-card__text-1 mb-1 text-black">
-                  Mulai dari <span className="text-danger fw-bold">IDR {flight.economyClass_price}</span>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          ))}
+          {data?.data
+            ?.filter((flight, index) => index >= 2 && index <= 18) // Filter data from index 2 to 18
+            .filter((flight) => {
+              if (selectedCategory === "") {
+                return true; // Tampilkan semua jika tidak ada filter kategori
+              }
+              const city = flight?.departureAirport?.city.toLowerCase();
+              const continent = dataWithContinent
+                .find((d) => d.city.toLowerCase() === city)
+                ?.continent.toLowerCase();
+              return continent === selectedCategory.toLowerCase();
+            })
+            .map((flight) => (
+              <Card
+                className="destinasi-card__list shadow mt-3"
+                style={{ cursor: "pointer" }}
+                key={flight.id}
+              >
+                <Card.Img
+                  variant="top"
+                  className="p-2 pb-0"
+                  src="/Frame-152.svg"
+                />
+                <Card.Body className="pt-2">
+                  <Card.Text className="destinasi-card__text-1 fw-bold mb-1">
+                    {flight?.departureAirport?.city} &rarr;{" "}
+                    {flight?.arrivalAirport?.city}
+                  </Card.Text>
+                  <Card.Text className="destinasi-card__text-2 fw-bold mb-1">
+                    {flight.airline.airline_name}
+                  </Card.Text>
+                  <Card.Text className="destinasi-card__text-2 mb-1 text-black">
+                    {Moment(flight.departure_time).format("DD")} -{" "}
+                    {Moment(flight.arrival_time).format("DD MMMM YYYY")}
+                  </Card.Text>
+                  <Card.Text className="destinasi-card__text-1 mb-1 text-black">
+                    Mulai dari{" "}
+                    <span className="text-danger fw-bold">
+                      IDR {flight.economyClass_price}
+                    </span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
         </div>
       </Container>
     </div>
